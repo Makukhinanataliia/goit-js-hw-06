72ddd2df40b01a31591d5563c83610e1851ae0c0
Makukhinanataliia <makukhinanataliia@gmail.com>
2023-12-26 16:18:03 +0330

    addComentsTask-1

--- js/task-1.js
+++ js/task-1.js
@@ -1,27 +1,37 @@
+// Об'єкт, що представляє клієнта з деякою інформацією
 const customer = {
-  username: "Mango",
-  balance: 24000,
-  discount: 0.1,
+  username: "Mango", 
+  balance: 24000, 
+  discount: 0.1, 
   orders: ["Burger", "Pizza", "Salad"],
 
+  // Метод повертає баланс користувача
   getBalance() {
     return this.balance;
   },
+
+  // Метод повертає поточну знижку на товари. 
   getDiscount() {
     return this.discount;
   },
+
+  // Метод встановлює нове значення знижки на товари.
   setDiscount(value) {
     this.discount = value;
   },
+
+  // Метод повертає список замовлень.
   getOrders() {
     return this.orders;
   },
+
+  // Метод додає нове замовлення та оновлює баланс користувача.
   addOrder(cost, order) {
-    this.balance -= cost - cost * this.discount;
-    this.orders.push(order);
+    this.balance -= cost - cost * this.discount; // Зменшує баланс з урахуванням знижки
+    this.orders.push(order); // Додає нове замовлення до списку
   },
 };
-customer.setDiscount(0.15);
+
 console.log(customer.getDiscount()); // 0.15
 customer.addOrder(5000, "Steak");
 console.log(customer.getBalance()); // 19750
