766859a7c3b09ab20c0446a7a8337064a0a90243
Makukhinanataliia <makukhinanataliia@gmail.com>
2023-12-26 16:08:56 +0330

    fixTask3

--- js/task-3.js
+++ js/task-3.js
@@ -18,10 +18,42 @@ class StringBuilder {
   }
 
   padBoth(str) {
-    this.padStart(str);
-    this.padEnd(str);
+    this.#value = str + this.#value + str; 
   }
 }
+const builder = new StringBuilder(".");
+console.log(builder.getValue()); // "."
+builder.padStart("^");
+console.log(builder.getValue()); // "^."
+builder.padEnd("^");
+console.log(builder.getValue()); // "^.^"
+builder.padBoth("=");
+console.log(builder.getValue()); // "=^.^="
+
+class StringBuilder {
+  #value;
+
+  constructor(initialValue) {
+    this.#value = initialValue;
+  }
+
+  getValue() {
+    return this.#value;
+  }
+
+  padEnd(str) {
+    this.#value += str;
+  }
+
+  padStart(str) {
+    this.#value = str + this.#value;
+  }
+
+  padBoth(str) {
+    this.#value = str + this.#value + str; // Combine the strings in one step
+  }
+}
+
 const builder = new StringBuilder(".");
 console.log(builder.getValue()); // "."
 builder.padStart("^");
